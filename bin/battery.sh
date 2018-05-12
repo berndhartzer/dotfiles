e03d9d8fd9f49d9008d0fd45772b561335c7fec3
Bernd Hartzer <berndhartzer@gmail.com>
2018-05-12 16:31:11 +1000

    updated battery script for linux

--- bin/battery.sh
+++ bin/battery.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 # Macos
-pmset -g batt | tail -1 | awk '{print $3}' | tr -d ';' | tr -d '%'
+if [ "$(uname)" == "Darwin" ]; then
+    pmset -g batt | tail -1 | awk '{print $3}' | tr -d ';' | tr -d '%'
+fi
+
+# Linux
+if [ "$(uname)" == "Linux" ]; then
+    upower -i $(upower -e | grep 'BAT') | grep -E "percentage" | awk '{print $2}' | tr -d '%'
+fi
